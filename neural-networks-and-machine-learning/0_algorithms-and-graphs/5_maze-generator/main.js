f7aa1903e9a33cf8658748d441baa91752a7f59c
Digicrest <46288957+Digicrest@users.noreply.github.com>
2019-03-28 11:04:13 +0000

    walls toggleable by boolean

--- neural-networks-and-machine-learning/0_algorithms-and-graphs/5_maze-generator/main.js
+++ neural-networks-and-machine-learning/0_algorithms-and-graphs/5_maze-generator/main.js
@@ -28,10 +28,24 @@ function Cell(x, y) {
   this.x = x * w;
   this.y = y * w;
 
+  // Top -> Right -> Bottom -> Left
+  this.walls = [true, true, true, true];
+
   this.show = function() {
-    line(this.x, this.y, this.x + w, this.y);         // TOP
-    line(this.x + w, this.y, this.x + w, this.y + w); // RIGHT
-    line(this.x, this.y + w, this.x + w, this.y + w); // BOTTOM
-    line(this.x, this.y, this.x, this.y + w)          // LEFT
+    if (this.walls[0]) {
+      line(this.x, this.y, this.x + w, this.y);
+    }
+    
+    if (this.walls[1]) {
+      line(this.x + w, this.y, this.x + w, this.y + w);
+    }
+    
+    if (this.walls[2]) {
+      line(this.x, this.y + w, this.x + w, this.y + w);
+    }
+    
+    if (this.walls[3]) {
+      line(this.x, this.y, this.x, this.y + w)
+    }
   }
 }
