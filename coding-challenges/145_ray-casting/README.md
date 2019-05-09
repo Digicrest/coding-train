a4f3be355dea741c4acdb7e9aab0f3a11b3d5f84
Digicrest <46288957+Digicrest@users.noreply.github.com>
2019-05-09 22:07:38 +0100

    2D raycasting

---
+++ coding-challenges/145_ray-casting/README.md
@@ -0,0 +1,4 @@
+<h3>Preview</h3>
+<img src="https://github.com/Digicrest/coding-train/blob/master/coding-challenges/145_ray-casting/preview.png"/>
+
+Tutorial: https://www.youtube.com/watch?v=TOEi6T2mtHo
---
+++ coding-challenges/145_ray-casting/particle.js
@@ -0,0 +1,47 @@
+class Particle {
+    constructor() {
+      this.pos = createVector(width / 2, height / 2);
+      this.rays = [];
+  
+      for (let ang = 0; ang < 360; ang += 1) {
+        this.rays.push(new Ray(this.pos, radians(ang)));
+      }
+    }
+  
+    update(x, y) {
+      this.pos.set(x, y);
+    }
+  
+    look(walls) {
+      for (let ray of this.rays) {
+        let closest = null;
+        let record = Infinity;
+  
+        for (let wall of walls) {
+          const pt = ray.cast(wall);
+  
+          if (pt) {
+            const d = p5.Vector.dist(this.pos, pt);
+            if (d < record) {
+              record = d;
+              closest = pt;
+            }
+          }
+        }
+  
+        if (closest) {
+          stroke(255, 100);
+          line(this.pos.x, this.pos.y, closest.x, closest.y)
+        }
+      }
+    }
+  
+    show() {
+      fill(255);
+      ellipse(this.pos.x, this.pos.y, 16);
+  
+      for (let ray of this.rays) {
+        ray.show();
+      }
+    }
+  }
