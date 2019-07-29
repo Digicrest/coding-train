ec33fdac7cb5d0473991f35b1335f2bb57af35b1
Kaylan Walker <kaylan_walker@damibu.com>
2019-07-29 08:36:34 +0100

    basic functionality working

--- coding-challenges/149_tic-tac-toe/app.js
+++ coding-challenges/149_tic-tac-toe/app.js
@@ -1,18 +1,22 @@
-const win_conditions = 
+const cloneDeep = obj => JSON.parse(JSON.stringify(obj))
+
+const initial_state = {
+    history: [],
+    board: [
+        ['', '', ''],
+        ['', '', ''],
+        ['', '', '']
+    ],
+    x_is_next: true,
+    game_over: false,
+    
+    error_message: '',
+    victory_message: ''
+};
+
 new Vue({
     el: '#app',
-
-    data: {
-        history: [],
-        board: [
-            ['', '', ''],
-            ['', '', ''],
-            ['', '', '']
-        ],
-        x_is_next: true,
-        game_over: false,
-        errormessage: ""
-    },
+    data: cloneDeep(initial_state),
     methods: {
         move: function (i, j) {
             const valid_move = this.checkValidMove(i, j)
@@ -20,45 +24,74 @@ new Vue({
             if (valid_move) {
                 // Set new Board State
                 const new_board = this.board.slice()
-                new_board[i][j] = this.x_is_next ? 'X' : 'O'
+                new_board[i][j] = this.x_is_next ? 'O' : 'X'
                 this.board = new_board
 
                 // Toggle Player
                 this.x_is_next = !this.x_is_next
+            } else {
+                alert(this.error_message)
             }
         },
 
         // position = { i: Number, j: Number }
         checkValidMove: function (x, y) {
+            this.error_message = ""
+
             if (this.board[x][y]) {
-                this.errormessage = "Cell is occupied."
+                this.error_message = "Cell is occupied."
                 return false
             }
 
-            this.errormessage = ""
+            if (this.game_over) {
+                this.error_message = "Game has already ended."
+                return false
+            }
+
             return true;
         },
 
-        checkGameOver: () => {
-            if ([...new Set(this.board[0]).length === 1].length === 1) {
-                // won by row
+        checkGameOver: function () {
+            // Horizontal Victory
+            this.board.forEach((row, i) => {
+                if (row[0] && row[0] === row[1] && row[1] === row[2]) {
+                    this.victory_message = 'Horizontal'
+                    this.game_over = true
+                }
+            });
+
+            // Vertical Victory
+            for (let i = 0; i < this.board.length; i++) {
+                const top = this.board[0][i];
+                const mid = this.board[1][i];
+                const bot = this.board[2][i];
+
+                if (top && top === mid && top === bot) {
+                    this.victory_message = 'Vertical'
+                    this.game_over = true
+                }
             }
-            // this.board.filter(arr => {
-            //     arr.sort();
-            //     arr[0] === arr
-            // }
+
+            // Diagonal Victory
+            const top_right_to_bottom_left = this.board[0][2] && this.board[0][2] === this.board[1][1] && this.board[0][2] === this.board[2][0]
+            const top_left_to_bottom_right = this.board[0][0] && this.board[0][0] === this.board[1][1] && this.board[0][0] === this.board[2][2]
+            
+            if (top_left_to_bottom_right || top_right_to_bottom_left) {
+                this.victory_message = 'Diagonal'
+                    ? [this.board[0][2], this.board[1][1], this.board[2][0]] 
+                    : [this.board[0][0], this.board[1][1], this.board[2][2]]
+                this.game_over = true
+            }
+
+            if (this.game_over) {
+                this.victory_message += ` Victory for Player ${ this.x_is_next ? 'X' : 'O' }`
+            }
         }
     },
     watch: {
-      board: new_board => {
-        this.history = [...history.slice(), new_board]
-        checkGameOver(new_board)
+      board: function () {
+        this.history = [...this.history, cloneDeep(this.board)]
+        this.checkGameOver()
       }
     }
 })
-
-
-// win conditions
-    // x val unchanged for 3 steps
-    // y val unchanges for 3 steps
-    // x and y delta is 1 for 3 steps
